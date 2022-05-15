724199e8b0c04b1f9e94721dc19cef1f87f357e9
Serhiy <SerhiyPetelin@gmail.com>
2022-05-15 20:19:10 +0000

    Add fancy-box js and css files for video playing

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').toggleClass('menu__list--active')
+    });
+
+    $('[data-fancybox]').fancybox({
+        youtube : {
+            controls : 1,
+            showinfo : 1
+        }
+    });
