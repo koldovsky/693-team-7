0eb6a3c6f3e06c4c87f0ce8c0e1e644b39d75110
Paul Sokolovskiy <paulsokolovskiy@gmail.com>
2022-05-25 12:23:25 +0000

    block slider section ready

--- js/main.js
+++ js/main.js
@@ -18,6 +18,8 @@ $(function () {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: false,
-        infinite: true
+        infinite: true,
+        prevArrow: '<div class="slick-prev"><i class="fa-solid fa-chevron-left"></i></div>',
+        nextArrow: '<div class="slick-next"><i class="fa-solid fa-chevron-right"></i></div>'
     })
 })
