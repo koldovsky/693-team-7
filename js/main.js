20a73cec405912d9445a4d060e9f856bc0d431f9
Paul Sokolovskiy <paulsokolovskiy@gmail.com>
2022-05-25 11:02:44 +0000

    added basic blog slider implementation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
-$(function(){
+$(function () {
 
-    $('.menu__btn').on('click', function() {
+    $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active')
     });
 
     $('[data-fancybox]').fancybox({
-        youtube : {
-            controls : 1,
-            showinfo : 1
+        youtube: {
+            controls: 1,
+            showinfo: 1
         }
     });
 });
+
+// Init blog slider
+$(function () {
+    $("#blog-slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        infinite: true
+    })
+})
