bfce4e69603722eb4af5be3fe14d62dddf8c84b3
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-05-26 22:07:29 +0000

    Added string of sale items

--- js/sale-items-list.js
+++ js/sale-items-list.js
@@ -1,3 +1,36 @@
 (function () {
-    
+    const itemsJson = `
+    [
+        {
+            "id": "1",
+            "imgURL": "img/sale/sale_adidas.jpeg",
+            "name": "Adidas Cloudfoam Trainers",
+            "price": 59.99
+        },
+        {
+            "id": "2",
+            "imgURL": "img/sale/sale_nike.jpeg",
+            "name": "Nike Classic Polo T-Shirt",
+            "price": 22.99
+        },
+        {
+            "id": "3",
+            "imgURL": "img/sale/sale_puma_top.jpeg",
+            "name": "Puma Essential Tank Top",
+            "price": 42.99
+        },
+        {
+            "id": "4",
+            "imgURL": "img/sale/sale_puma.jpeg",
+            "name": "Puma Fitness Shoes in Grey",
+            "price": 59.99
+        },
+        {
+            "id": "5",
+            "imgURL": "img/sale/sale_reebok.jpeg",
+            "name": "Reebok Workout Shorts",
+            "price": 24.99
+        }
+    ]
+    `;
 })()
