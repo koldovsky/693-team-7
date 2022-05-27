3d31638073a0fe73d0a28dd3658d8f85aaa8dedd
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-05-27 03:11:08 +0000

    Added html to sale-items-list.js file

--- js/sale-items-list.js
+++ js/sale-items-list.js
@@ -3,34 +3,50 @@
     [
         {
             "id": "1",
-            "imgURL": "img/sale/sale_adidas.jpeg",
+            "imgUrl": "img/sale/sale_adidas.jpeg",
             "name": "Adidas Cloudfoam Trainers",
             "price": 59.99
         },
         {
             "id": "2",
-            "imgURL": "img/sale/sale_nike.jpeg",
+            "imgUrl": "img/sale/sale_nike.jpeg",
             "name": "Nike Classic Polo T-Shirt",
             "price": 22.99
         },
         {
             "id": "3",
-            "imgURL": "img/sale/sale_puma_top.jpeg",
+            "imgUrl": "img/sale/sale_puma_top.jpeg",
             "name": "Puma Essential Tank Top",
             "price": 42.99
         },
         {
             "id": "4",
-            "imgURL": "img/sale/sale_puma.jpeg",
+            "imgUrl": "img/sale/sale_puma.jpeg",
             "name": "Puma Fitness Shoes in Grey",
             "price": 59.99
         },
         {
             "id": "5",
-            "imgURL": "img/sale/sale_reebok.jpeg",
+            "imgUrl": "img/sale/sale_reebok.jpeg",
             "name": "Reebok Workout Shorts",
             "price": 24.99
         }
     ]
     `;
+
+    function renderItems(items) {
+        const itemsContainer = document.querySelector('.sale__items');
+        for (const item of items) {
+            itemsContainer.innerHTML += `
+            <div class="sale__items-item a">
+                <a href="catalogue01.html" class="sale__items-item-img"> <img src="${item.imgUrl}"
+                alt="${item.name}"> </a>
+                <a href="catalogue01.html" class="sale__items-item-link">${item.name}</a>
+                <p class="sale__items-item-price">$ ${item.price}</p>
+                <button class="sale__items-item-button" type="button" data-bs-toggle="modal"
+                data-bs-target="#shopping-cart-modal"> Add to Cart</button>
+            </div>
+            `;
+        }
+    }
 })()
