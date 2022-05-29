a22aa4eb7e84dc84f897a2ae41559288e5023078
Paul Sokolovskiy <paulsokolovskiy@gmail.com>
2022-05-29 10:31:15 +0000

    blog-slider done

--- js/main.js
+++ js/main.js
@@ -20,6 +20,32 @@ $(function () {
         autoplay: false,
         infinite: true,
         prevArrow: '<div class="slick-prev"><i class="fa-solid fa-chevron-left"></i></div>',
-        nextArrow: '<div class="slick-next"><i class="fa-solid fa-chevron-right"></i></div>'
+        nextArrow: '<div class="slick-next"><i class="fa-solid fa-chevron-right"></i></div>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 450,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+        ]
     })
 })
