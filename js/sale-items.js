31b856676dac86bc34552c4a9aa60384a622c8ee
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-06-04 18:49:08 +0000

    Changed button for modal window

--- js/sale-items.js
+++ js/sale-items.js
@@ -1,32 +1,37 @@
-[
+const items = [
     {
-        "id": "1",
+        "id": 0,
         "imgUrl": "img/sale/sale_adidas.jpeg",
+        "smallImgUrl": "img/sale/shopping-cart-trainers.jpeg",
         "name": "Adidas Cloudfoam Trainers",
         "price": 59.99
     },
     {
-        "id": "2",
+        "id": 1,
         "imgUrl": "img/sale/sale_nike.jpeg",
+        "smallImgUrl": "img/sale/shopping-cart-trainers.jpeg",
         "name": "Nike Classic Polo T-Shirt",
         "price": 22.99
     },
     {
-        "id": "3",
+        "id": 2,
         "imgUrl": "img/sale/sale_puma_top.jpeg",
+        "smallImgUrl": "img/sale/shopping-cart-trainers.jpeg",
         "name": "Puma Essential Tank Top",
         "price": 42.99
     },
     {
-        "id": "4",
+        "id": 3,
         "imgUrl": "img/sale/sale_puma.jpeg",
+        "smallImgUrl": "img/sale/shopping-cart-trainers.jpeg",
         "name": "Puma Fitness Shoes in Grey",
         "price": 59.99
     },
     {
-        "id": "5",
+        "id": 4,
         "imgUrl": "img/sale/sale_reebok.jpeg",
+        "smallImgUrl": "img/sale/shopping-cart-trainers.jpeg",
         "name": "Reebok Workout Shorts",
         "price": 24.99
     }
-]
+];
