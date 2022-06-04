0c50c818ce4f05ec513210b56435bfa855c5ece0
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2022-06-04 19:06:16 +0000

    Added condition to check if product already exist in cart

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -1,16 +1,25 @@
 //(function () {
-  //  const shoppingBag = document.querySelector(".shopping-bag");
-    //const addToCart = document.querySelector(".sale__items-item-button");
-  
-    //shoppingBag.style.display = "none";
-  //})();
+//  const shoppingBag = document.querySelector(".shopping-bag");
+//const addToCart = document.querySelector(".sale__items-item-button");
+
+//shoppingBag.style.display = "none";
+//})();
 
 //cart array
 let cart = [];
 
 //Add to Cart
 function addToCart(id) {
-  const product = items.find((item) => item.id === id);
-  console.log(product);
+  // check if product already exist in cart
+  if (cart.some((product) => product.id === id)) {
+    alert("Product already in cart!");
+  } else {
+    const product = items.find((item) => item.id === id);
+
+    cart.push({
+      ...product,
+      numberOfUnits: 1,
+    });
+    console.log(cart);
+  }
 }
-  
