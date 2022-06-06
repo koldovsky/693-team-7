40bb572d1e41c48140c967a295958dc480b6bfac
Serhiitoo <serhiiromanchuk632@gmail.com>
2022-06-06 15:23:43 +0000

    serhii rom. caurosel

---
+++ js/products-caruesel-deal-of-the-month.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('.products').slick({
+		arrows:true,
+		dots:false,
+		slidesToShow:4,
+        prevArrow: '<div class="slick-prev slick-arrow"><i class="fa-solid fa-chevron-left"></i></div>',
+        nextArrow: '<div class="slick-next slick-arrow"><i class="fa-solid fa-chevron-right"></i></div>',
+		autoplay:true,
+		speed:1000,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
+
