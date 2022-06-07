2eee20fc35a3e9634afbf103c19979a0a7140256
Zelosc <eferst2012@gmail.com>
2022-06-07 13:13:18 +0000

    Fixed main-collection.js

---
+++ js/main-collection.js
@@ -0,0 +1,34 @@
+function menFunction(){
+    document.getElementById("main-collection__display-container__body-img__1").src='img/main-collection/boy_black-gray-clothes.jpg';
+    document.getElementById("main-collection__display-container__body-name__1").innerHTML="Adidas Compression Tee";
+    document.getElementById("main-collection__display-container__body-price__1").innerHTML="$33,00";
+    document.getElementById("main-collection__display-container__body-img__2").src='img/main-collection/boy_red-white-clothes.jpg';
+    document.getElementById("main-collection__display-container__body-name__2").innerHTML="Reebok Fitness Jacket";
+    document.getElementById("main-collection__display-container__body-price__2").innerHTML="$52,00";
+    document.getElementById("main-collection__display-container__body-img__3").src='img/main-collection/boy_white-orange-clothes.jpg';
+    document.getElementById("main-collection__display-container__body-name__3").innerHTML="Quicksilver Tech T-Shirt";
+    document.getElementById("main-collection__display-container__body-price__3").innerHTML="$21,00";
+}
+function womenFunction(){
+    document.getElementById("main-collection__display-container__body-img__1").src='img/main-collection/girl_black-clothes.jpg';
+    document.getElementById("main-collection__display-container__body-name__1").innerHTML="Puma Sports Bra";
+    document.getElementById("main-collection__display-container__body-price__1").innerHTML="$12,00";
+    document.getElementById("main-collection__display-container__body-img__2").src='img/main-collection/girl_pink-clothes.jpg';
+    document.getElementById("main-collection__display-container__body-name__2").innerHTML="Reebok Long Sleeve Top";
+    document.getElementById("main-collection__display-container__body-price__2").innerHTML="$18,00";
+    document.getElementById("main-collection__display-container__body-img__3").src='img/main-collection/girl_gray-clothes.jpg';
+    document.getElementById("main-collection__display-container__body-name__3").innerHTML="Quicksilver Workout Top";
+    document.getElementById("main-collection__display-container__body-price__3").innerHTML="$12,00";
+}
+function shoesFunction(){
+    document.getElementById("main-collection__display-container__body-img__1").src='img/main-collection/grey_shoes.jpg';
+    document.getElementById("main-collection__display-container__body-name__1").innerHTML="Nike Amanda Running Shoes";
+    document.getElementById("main-collection__display-container__body-price__1").innerHTML="$120,00";
+    document.getElementById("main-collection__display-container__body-img__2").src='img/main-collection/light_shoes.jpg';
+    document.getElementById("main-collection__display-container__body-name__2").innerHTML="Puma Basket Shoes";
+    document.getElementById("main-collection__display-container__body-price__2").innerHTML="$140,00";
+    document.getElementById("main-collection__display-container__body-img__3").src='img/main-collection/green_shoes.jpg';
+    document.getElementById("main-collection__display-container__body-name__3").innerHTML="Adidas Campus";
+    document.getElementById("main-collection__display-container__body-price__3").innerHTML="$130,00";
+}
+
